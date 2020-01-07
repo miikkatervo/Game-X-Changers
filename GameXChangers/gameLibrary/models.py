41a3a4ed0e609bc3acf06fa93aae4da494609336
Mäkkäri Pro <arttu.maatta@aalto.fi>
2020-01-07 08:57:33 +0200

    make initial Django Models

--- GameXChangers/gameLibrary/models.py
+++ GameXChangers/gameLibrary/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.db.models import Model
 
-# Create your models here.
+
+
+# Model User comes directly from django.contri.auth
+# with username as unique attribute
+ 
+
+class Game(models.Model):
+    name = models.CharField(max_length=30 , unique = True)
+    description = models.TextField
+    url = models.URLField
+    highscore = models.IntegerField
+    price = models.IntegerField
+    
+
+class Transaction(models.Model):
+    buyerName = models.CharField(max_length=30 , unique = True)
+    description = models.TextField
+    price = models.IntegerField
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class OwnedGame(models.Model):
+    user = models.CharField(max_length=30)
+    game = models.CharField(max_length=30)
+    #0-100
+    progress = models.IntegerField
+    highscore = models.IntegerField
+
+#only one developer per game, thus game is unique
+class DevelopedGame(models.Model):
+    user = models.CharField(max_length=30 )
+    game = models.CharField(max_length=30 , unique = True)
