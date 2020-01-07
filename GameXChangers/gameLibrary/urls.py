39d10c158ca72c0d12eca0895b7b16a3d538dd7e
Miikka Tervo <miikka.tervo@gmail.com>
2020-01-07 09:59:44 +0200

    add navbar

--- GameXChangers/gameLibrary/urls.py
+++ GameXChangers/gameLibrary/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('browseGames/', views.browseGames, name='browseGames'),
+    path('myGames/', views.myGames, name='myGames'),
 ]
