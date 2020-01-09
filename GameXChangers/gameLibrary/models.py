45de2f84b4587c8e36ce583f31d5c434ae5b7c70
Ekku Jokinen <ekku.jokinen@aalto.fi>
2020-01-09 11:11:56 +0200

    Make Game & OwnedGame work

--- GameXChangers/gameLibrary/models.py
+++ GameXChangers/gameLibrary/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
+
+
+class AutoDateTimeField(models.DateTimeField):
+    def pre_save(self, model_instance, add):
+        return timezone.now()
+
 
 # Model User comes directly from django.contri.auth.
 # https://docs.djangoproject.com/en/3.0/ref/contrib/auth/
 
-default_user = user = User.objects.create_user('Mike', 'Mike@email.com', 'mikepassword1')
+
 class Game(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.TextField(default='Game Description')
@@ -13,22 +20,18 @@ class Game(models.Model):
     price = models.IntegerField(default=0)
 
     # May be more efficient to store the "player-game" -relation
-    # in the User-model. To be considered.
-    players = models.ManyToManyField(User, through='OwnedGame')
-    developer = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='developed_game',
-        default='')
+    # in the User-model. To be considered
+    players = models.ManyToManyField(User, related_name='games',through='OwnedGame')
+    developer = models.ForeignKey(User, related_name='game_developed', on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.name
 
 
 class OwnedGame(models.Model):
-    player = models.ForeignKey(User, on_delete=models.CASCADE)
+    player = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    bought_at = models.DateTimeField(auto_now_add=True)
+    bought_at = models.DateTimeField(default=timezone.now)
     # 0-100
     progress = models.IntegerField(default=0)
     highscore = models.IntegerField(default=0)
