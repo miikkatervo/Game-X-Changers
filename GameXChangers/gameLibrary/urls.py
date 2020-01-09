e33c7d74f881bd797d3288662b8d578ba36bd74c
Miikka Tervo <miikka.tervo@gmail.com>
2020-01-09 19:36:09 +0200

    user can add a game

--- GameXChangers/gameLibrary/urls.py
+++ GameXChangers/gameLibrary/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('browseGames/', views.browseGames, name='browseGames'),
     path('myGames/', views.myGames, name='myGames'),
     path('playGame/<int:game_id>', views.playGame, name='playGame'),
+    path('addGame/', views.addGame, name='addGame'),
 ]
--- GameXChangers/gameLibrary/views.py
+++ GameXChangers/gameLibrary/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Game, OwnedGame
 from django.contrib.auth.models import User
+from .forms import GameForm
 
 # Create your views here.
 
@@ -8,10 +9,13 @@ def home(request):
     return render(request, 'gameLibrary/home.html')
 
 def browseGames(request):
-    game_list = Game.objects.all()
-    owned_game_objects = list(filter(lambda x: x.player == request.user, OwnedGame.objects.all()))
-    owned_game_list = list(map(lambda x: x.game, owned_game_objects))
-    context = {'game_list': game_list, 'owned_game_list': owned_game_list}
+    try:
+        game_list = Game.objects.all()
+        owned_game_objects = list(filter(lambda x: x.player == request.user, OwnedGame.objects.all()))
+        owned_game_list = list(map(lambda x: x.game, owned_game_objects))
+        context = {'game_list': game_list, 'owned_game_list': owned_game_list}
+    except:
+        raise Http404("Game does not exist")
     return render(request, 'gameLibrary/browseGames.html', context)
 
 def myGames(request):
@@ -24,5 +28,19 @@ def playGame(request, game_id):
     try:
         game = Game.objects.get(pk=game_id)
     except Game.DoesNotExist:
-        raise Http404("Question does not exist")
+        raise Http404("Game does not exist")
     return render(request, 'gameLibrary/playGame.html', { 'game': game })
+
+
+def addGame(request):
+    if request.method == 'POST':
+        form = GameForm(request.POST)
+        if form.is_valid():
+            game = form.save(commit=False)
+            game.developer = request.user
+            game.save()
+    
+    else:
+        form = GameForm()
+
+    return render(request, 'gameLibrary/addGame.html', {'form': form})
